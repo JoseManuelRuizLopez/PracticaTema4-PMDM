6220265c1a1461f4072b3bdc7a84f4c92b65b8e1
JoseManuelRuizLopez <Josemanuelruizlopez97@gmail.com>
2020-01-03 18:09:29 +0100

    Commit aplicación traducida a dos idiomas

--- app/src/main/java/com/example/practicatema4/JuegoAciertos.java
+++ app/src/main/java/com/example/practicatema4/JuegoAciertos.java
@@ -10,5 +10,7 @@ public class JuegoAciertos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_juego_aciertos);
+
+
     }
 }
